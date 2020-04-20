8c686feb3a435497962bd19f12e2d0e450abaf15
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-04-20 16:34:07 +0900

    (shortcuts : res : medical) periodical updates

---
+++ start_korean.bat
@@ -0,0 +1,31 @@
+REM =================================
+REM 	log file
+REM 	git
+REM 	browser
+REM 	log dir
+REM 	terminal
+REM =================================
+@echo off
+
+REM opening message
+echo starting start_korean.bat // C:\Users\iwabuchiken\Desktop\shortcuts_docs
+
+
+REM =================================
+REM 	browser
+REM 	2020/01/07 14:44:50
+REM =================================
+pushd C:\WORKS_2\Programs\opera
+
+set url_1="https://mail.google.com/mail/ca/u/0/#search/myself+korean?compose=new"
+
+set url_2=https://stopwatch-app.com
+set url_3=http://benfranklin.chips.jp/PHP_server/D-2/time_calc.php
+
+launcher.exe %url_1% %url_2% %url_3%
+
+
+echo.>> C:\Users\iwabuchiken\Desktop\shortcuts_docs\start_korean.bat
+
+pause
+
