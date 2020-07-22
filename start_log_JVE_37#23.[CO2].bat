492ef672a8cebfa3856acbf42a0db8914868dbd9
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-07-22 13:34:16 +0900

    (shortcuts : res : history_arab-persia) periodical updates

--- start_log_JVE_37#23.[CO2].bat
+++ start_log_JVE_37#23.[CO2].bat
@@ -3,8 +3,10 @@ REM 	folders
 REM 	doc
 REM 	app : mspaint
 REM 	git
-REM 	time label
 REM 	browser
+REM 	map
+REM 	update : bat file
+REM 	time label
 REM ==============================
 
 REM ==============================
@@ -34,20 +36,37 @@ REM C:\WORKS_2\batches\s.bat
 REM sakura C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\41_economics\7_topics\log_JVE_41.[economics].txt
 
 REM ==============================
+REM 	browser
+REM 	2020年5月23日17:44:58
+REM ==============================
+rem pushd "C:\WORKS_2\Programs\Tor Browser\Browser"
+rem start firefox.exe
+
+pushd C:\WORKS_2\Programs\opera
+
+set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=co2&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+
+launcher.exe %url_1%
+
+REM ==============================
+REM 	map
+REM 	2020年7月21日18:20:38
+REM ==============================
+start C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\37_miscs\23_co2\1_mind-map.mm
+
+
+REM ==============================
+REM 	update : bat file
+REM 	2020年7月21日18:12:20
+REM ==============================
+rem echo.>> "C:\Users\iwabuchiken\Desktop\shortcuts_docs\start_log_JVE_37#23.[CO2].bat"
+echo.>> "C:\WORKS_2\shortcuts_docs\start_log_JVE_37#23.[CO2].bat"
+
+REM ==============================
 REM 	time label
 REM 	2020年5月16日15:58:57
 REM ==============================
 C:\WORKS_2\t.bat
-
-REM ==============================
-REM 	browser
-REM 	2020年5月23日17:44:58
-REM ==============================
-pushd "C:\WORKS_2\Programs\Tor Browser\Browser"
-start firefox.exe
-
-rem echo.>> "C:\Users\iwabuchiken\Desktop\shortcuts_docs\start_log_JVE_37#23.[CO2].bat"
-echo.>> "C:\WORKS_2\shortcuts_docs\start_log_JVE_37#23.[CO2].bat"
 
 
 
--- start_log_JVE_44_10_2.[fx-php-tester].bat
+++ start_log_JVE_44_10_2.[fx-php-tester].bat
@@ -144,3 +144,4 @@ REM C:\WORKS_2\a && cd C:\Users\iwabuchiken\Desktop\shortcuts_docs && g a . && g
 
 
 
+
