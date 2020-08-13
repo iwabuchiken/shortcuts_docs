3430e4cc78feecfdd382f8e5dfa9ee8fd5aff6f6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-08-14 08:33:14 +0900

    (shortcuts : dance) periodical updates

---
+++ start_music-composition.bat
@@ -0,0 +1,13 @@
+@echo on
+
+REM =================================
+REM 	update batch file
+REM 	2020”N8ŒŽ13“ú22:35:03
+REM =================================
+echo.>> C:\WORKS_2\shortcuts_docs\start_music-composition.bat
+
+REM =================================
+REM 	batch file
+REM 	2020”N8ŒŽ13“ú22:35:28
+REM =================================
+C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\start_music-composition.bat
