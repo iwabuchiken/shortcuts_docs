2c1787c55bed401626c2dd4d2627b52c2922b500
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-08-13 17:37:13 +0900

    (WS_Others.JVEMV6 : math : knot theory) periodical updates

--- start_knot-theory.bat
+++ start_knot-theory.bat
@@ -6,12 +6,6 @@ REM 	browser
 REM 	folders
 REM =================================
 
-REM =================================
-REM 	txt file
-REM 	2020年7月21日14:34:54
-REM =================================
-rem sakura "C:\WORKS_2\shortcuts_docs\log_JVE_37#21.4.[math_knot-theory].txt"
-C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\21_math&physics\4_knot-theory\log_JVE_37#21.4.[math_knot-theory].txt"
 
 REM =================================
 REM 	map
@@ -43,12 +37,22 @@ REM 	2020年7月21日14:34:41
 REM =================================
 echo.>> C:\WORKS_2\shortcuts_docs\start_knot-theory.bat
 
+REM =================================
+REM 	txt file
+REM 	2020年7月21日14:34:54
+REM =================================
+rem sakura "C:\WORKS_2\shortcuts_docs\log_JVE_37#21.4.[math_knot-theory].txt"
+C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\21_math&physics\4_knot-theory\log_JVE_37#21.4.[math_knot-theory].txt"
+
+
+
 ======================== DOCS ========================
 https://yandex.ru/images/search?from=tabbar&text=もやい結び&pos=0&img_url=https://blogimg.goo.ne.jp/user_image/58/74/f6d2b4f2c0ad9d6815395f8c5d7546fe.jpg&rpt=simage	2020年7月30日23:28:55
 
 
 
 ======================== EOF ========================
+
 
 
 
