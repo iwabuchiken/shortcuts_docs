7623a60853257d1763d76912372e4daa7cae775a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-08-15 16:49:56 +0900

    (shortcuts : anima-dessin) periodical updates

---
+++ start_[anima-dessin].bat
@@ -0,0 +1,24 @@
+
+REM =================================
+REM 	folders
+REM 	2020年8月15日16:20:41
+REM =================================
+start C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+
+REM =================================
+REM 	browser
+REM 	2020年8月15日16:18:51
+REM =================================
+pushd C:\WORKS_2\Programs\opera
+
+set url_1="http://benfranklin.chips.jp/WS/WS_Others/free/UH8G6E_CE/1_X/1_14/main.(1.14~).php"
+set url_2="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=anim&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+
+launcher.exe %url_1% %url_2%
+
+
+REM =================================
+REM 	update batch file
+REM 	2020年8月15日16:18:55
+REM =================================
+echo.>> C:\WORKS_2\shortcuts_docs\start_[anima-dessin].bat
