eb1ef4e5679ac733a97272c8da9a21b44cb902d4
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-08-16 16:01:06 +0900

    (WS_Others.JVEMV6 : math : knot theory) periodical updates

--- start_music-composition.bat
+++ start_music-composition.bat
@@ -1,4 +1,8 @@
 @echo on
+REM =================================
+REM 	update batch file
+REM 	batch file
+REM =================================
 
 REM =================================
 REM 	update batch file
@@ -10,5 +14,6 @@ REM =================================
 REM 	batch file
 REM 	2020”N8ŒŽ13“ú22:35:28
 REM =================================
-C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\start_music-composition.bat
+start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\start_music-composition.bat
+
 
