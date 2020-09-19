50e06cc4ff1bd535e9877a760d7ae7af6072b5a3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-09-19 09:28:25 +0900

    (shortcuts : [music] : 2020-09-19_07-22-28_000) periodical updates

---
+++ end_[music].bat
@@ -0,0 +1,14 @@
+REM ==============================
+REM 	log file
+REM 
+REM 	git : shortcuts : [music]
+REM 
+REM 	2020年9月19日9:07:29
+REM ==============================
+C:\WORKS_2\a.bat && pushd C:\WORKS_2\shortcuts_docs && git add . && git commit -m "(shortcuts : [music]) periodical updates" && e && git push origin master && r oj && git add . && git commit -m "(JVE_46-XX.[music]) periodical updates" && e && p
+
+rem ========================= EOF ========================
+
+pause
+
+
---
+++ start_muse-3.bat
@@ -0,0 +1,29 @@
+REM ==============================
+REM 	taskkill
+REM 	2020年9月19日9:00:35
+REM ==============================
+echo taskkill /f /im MuseScore3.exe
+taskkill /f /im MuseScore3.exe
+
+REM ==============================
+REM 	start
+REM 	2020年9月19日9:00:35
+REM ==============================
+echo start C:\WORKS_2\Programs\musescore\ms_3.2.3.7635\bin\MuseScore3.exe
+start C:\WORKS_2\Programs\musescore\ms_3.2.3.7635\bin\MuseScore3.exe
+
+REM ==============================
+REM 	taskkill
+REM 	2020年9月19日9:00:35
+REM ==============================
+echo taskkill /f /im MuseScore3.exe
+taskkill /f /im MuseScore3.exe
+
+REM ==============================
+REM 	start
+REM 	2020年9月19日9:00:35
+REM ==============================
+echo start C:\WORKS_2\Programs\musescore\ms_3.2.3.7635\bin\MuseScore3.exe
+start C:\WORKS_2\Programs\musescore\ms_3.2.3.7635\bin\MuseScore3.exe
+
+pause
