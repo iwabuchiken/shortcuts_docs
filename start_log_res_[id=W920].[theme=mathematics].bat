6334f892f555941a506a96956a7622c63816185f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-10 11:52:04 +0900

    (shortcuts : [id=W920].[theme=mathematics]) periodical updates

---
+++ start_log_res_[id=W920].[theme=mathematics].bat
@@ -0,0 +1,54 @@
+REM =================================
+REM 	txt file
+REM 	map
+REM 	update batch file
+REM 	browser
+REM 	folders
+REM =================================
+
+
+REM =================================
+REM 	map
+REM 	2020年7月21日14:36:38
+REM =================================
+start C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\37_miscs\W920mathematics\W920_mathematics.mm
+
+
+REM =================================
+REM 	browser
+REM 	2020年7月21日14:38:25
+REM =================================
+pushd C:\WORKS_2\Programs\opera
+
+set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=W920&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+
+launcher.exe %url_1%
+
+
+REM =================================
+REM 	folders
+REM 	2020年7月21日14:40:24
+REM =================================
+start C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+
+REM =================================
+REM 	update batch file
+REM 	2020年7月21日14:34:41
+REM =================================
+echo.>> start_log_res_[id=W920].[theme=mathematics].bat
+
+REM =================================
+REM 	txt file
+REM 	2020年7月21日14:34:54
+REM =================================
+rem sakura "C:\WORKS_2\shortcuts_docs\log_JVE_37#21.4.[math_knot-theory].txt"
+rem C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\21_math&physics\4_knot-theory\log_JVE_37#21.4.[math_knot-theory].txt"
+"C:\WORKS_2\shortcuts_docs\log_res_[id=W920].[theme=mathematics].[20201007_094538].txt"
+
+
+
+======================== DOCS ========================
+
+
+
+======================== EOF ========================
