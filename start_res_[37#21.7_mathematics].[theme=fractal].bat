59bf0ca247a25698a849a469833c5eb0280c76da
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-25 22:53:52 +0900

    (shortcuts : res_[37#21.8_physics].[theme=gauge-theory]) periodical updates

---
+++ start_res_[37#21.7_mathematics].[theme=fractal].bat
@@ -0,0 +1,57 @@
+REM =================================
+REM 	log file
+REM 	folders
+REM 	browser
+REM 	freemind
+REM 	update batch file
+REM =================================
+@echo off
+
+REM opening message
+echo res_[37#21.7_mathematics].[theme=fractal]...
+
+REM =================================
+REM 	update batch file
+REM 	2020年10月24日13:20:36
+REM =================================
+echo.>> "C:\WORKS_2\shortcuts_docs\start_res_[37#21.7_mathematics].[theme=fractal].bat"
+
+REM =================================
+REM 	log file
+REM 	2020年10月22日10:03:40
+REM =================================
+pushd C:\WORKS_2\shortcuts_docs
+start log_res_[37#21.7_mathematics].[theme=fractal].[20201020_152846].txt
+
+REM =================================
+REM 	folders
+REM 	2020年10月25日10:13:58
+REM =================================
+start C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+
+
+REM =================================
+REM 	browser
+REM 	2020年10月22日10:04:11
+REM =================================
+pushd C:\WORKS_2\Programs\opera
+
+rem set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=37%%#21.7&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND&RBs_AND_OR_File_Name=AND"
+rem set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=37^#21.7&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND&RBs_AND_OR_File_Name=AND"
+rem set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=37^%21.7&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND&RBs_AND_OR_File_Name=AND"
+rem set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=37^%%21.7&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND&RBs_AND_OR_File_Name=AND"
+set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=jve+37+21.7+fractal&RBs_AND_OR_Memo=AND"
+launcher.exe %url_1%
+
+REM =================================
+REM 	freemind
+REM 	2020年10月22日10:07:04
+REM =================================
+start C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\37_miscs\21_math-physics\7_fractal.mm
+
+rem pause
+
+
+
+
+
--- start_res_[37#21.8_physics].[theme=gauge-theory].bat
+++ start_res_[37#21.8_physics].[theme=gauge-theory].bat
@@ -8,20 +8,20 @@ REM =================================
 @echo off
 
 REM opening message
-echo res_[37#21.6_mathematics].[theme=fractal]...
+echo res_[37#21.8_physics].[theme=gauge-theory]...
 
 REM =================================
 REM 	update batch file
 REM 	2020”N10ŒŽ24“ú13:20:36
 REM =================================
-echo.>> "C:\WORKS_2\shortcuts_docs\start_res_[37#21.6_mathematics].[theme=fractal].bat"
+echo.>> "C:\WORKS_2\shortcuts_docs\start_res_[37#21.8_physics].[theme=gauge-theory].bat"
 
 REM =================================
 REM 	log file
 REM 	2020”N10ŒŽ22“ú10:03:40
 REM =================================
 pushd C:\WORKS_2\shortcuts_docs
-start log_res_[37#21.6_mathematics].[theme=fractal].[20201020_152846].txt
+start log_res_[37#21.8_physics].[theme=gauge-theory].[20201025_225007].txt
 
 REM =================================
 REM 	folders
@@ -40,14 +40,14 @@ rem set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?f
 rem set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=37^#21.7&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND&RBs_AND_OR_File_Name=AND"
 rem set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=37^%21.7&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND&RBs_AND_OR_File_Name=AND"
 rem set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=37^%%21.7&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND&RBs_AND_OR_File_Name=AND"
-set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=jve+37+21.7+fractal&RBs_AND_OR_Memo=AND"
+set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=jve+37+21.8+gauge&RBs_AND_OR_Memo=AND"
 launcher.exe %url_1%
 
 REM =================================
 REM 	freemind
 REM 	2020”N10ŒŽ22“ú10:07:04
 REM =================================
-start C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\37_miscs\21_math-physics\7_fractal.mm
+start C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\37_miscs\21_math-physics\8_gauge-theory.mm
 
 rem pause
 
