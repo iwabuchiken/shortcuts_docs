e0d51b74f83d823fcf1ec3aaf1a003e4585fa549
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-29 01:30:40 +0900

    (shortcuts : JVE_37-17.[natural-history] : id=23) periodical updates

---
+++ start_log_[drink-alcohol].bat
@@ -0,0 +1,34 @@
+REM =================================
+REM 	log file
+REM 	folders
+REM 	browser
+REM 	freemind
+REM 	update batch file
+REM =================================
+@echo off
+
+REM opening message
+echo log_[drink-alcohol].txt ==^> starting...
+
+rem goto end
+
+REM =================================
+REM 	update batch file
+REM 	2020年10月24日13:20:36
+REM =================================
+echo.>> "C:\WORKS_2\shortcuts_docs\start_log_[drink-alcohol].bat"
+
+REM =================================
+REM 	log file
+REM 	2020年10月22日10:03:40
+REM =================================
+pushd C:\WORKS_2\shortcuts_docs
+"res_[theme=drink-alcohol]_[id=1MPF]_[20201005_004110].ods"
+
+"log_[drink-alcohol].txt"
+
+
+:end
+pause
+
+
