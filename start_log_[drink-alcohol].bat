f447c73cb33f61bca3bd78c02bc77b1f67163a55
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-31 00:31:20 +0900

    (shortcuts : JVE_37-17.[natural-history] : id=23) periodical updates

--- start_log_[drink-alcohol].bat
+++ start_log_[drink-alcohol].bat
@@ -18,17 +18,28 @@ REM 	2020年10月24日13:20:36
 REM =================================
 echo.>> "C:\WORKS_2\shortcuts_docs\start_log_[drink-alcohol].bat"
 
+echo batch file --^> updated
+
 REM =================================
 REM 	log file
 REM 	2020年10月22日10:03:40
 REM =================================
 pushd C:\WORKS_2\shortcuts_docs
+
+echo opening the ods file...
+
 "res_[theme=drink-alcohol]_[id=1MPF]_[20201005_004110].ods"
+
+echo opening the ods file...complete
+
+echo opening the log file...
 
 "log_[drink-alcohol].txt"
 
+echo opening the log file...complete
 
 :end
 rem pause
+
 
 
