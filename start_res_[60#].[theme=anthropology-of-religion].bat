323ca27ec56659d3082d092b2b4263672379817a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-11-13 18:55:18 +0900

    (shortcuts : log_[flute]) periodical updates

--- start_res_[60#].[theme=anthropology-of-religion].bat
+++ start_res_[60#].[theme=anthropology-of-religion].bat
@@ -1,3 +1,5 @@
+REM res_[60#].[theme=anthropology-of-religion]
+
 REM =================================
 REM 	update batch file
 REM 	browser
@@ -7,26 +9,26 @@ REM =================================
 @echo off
 
 REM opening message
-echo start_log_JVE_37-20.[philosophy].bat ==^> starting...
+echo start_res_[60#].[theme=anthropology-of-religion].bat ==^> starting...
 
 rem goto end
 
 REM =================================
 REM 	update batch file
-REM 	2020年10月24日13:20:36
+REM 	2020年11月13日16:43:57
 REM =================================
-echo.>> "C:\WORKS_2\shortcuts_docs\start_log_JVE_37-20.[philosophy].bat"
+echo.>> "C:\WORKS_2\shortcuts_docs\start_res_[60#].[theme=anthropology-of-religion].bat"
 
 echo batch file --^> updated
 
 REM =================================
 REM 	browser
-REM 	2020年11月2日15:02:53
+REM 	2020年11月13日16:46:20
 REM =================================
 
 pushd C:\WORKS_2\Programs\opera
 
-set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=37+20+philosophy&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=jve+anthropology-of-religion&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
 
 echo opening browser... : %url_1%
 
@@ -34,11 +36,11 @@ launcher.exe %url_1%
 
 REM =================================
 REM 	freemind
-REM 	2020年11月2日15:03:29
+REM 	2020年11月13日16:47:29
 REM =================================
 echo starting... freemind
 
-"C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\37_miscs\20_philosophy\20_philosophy.mm"
+"C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\60_anthropology-of-religion\60_anthropology-of-religion.mm"
 
 REM =================================
 REM 	folders
@@ -50,11 +52,11 @@ REM =================================
 REM 	log file
 REM 	2020年10月22日10:03:40
 REM =================================
-pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\20_philosophy
+pushd C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\60_anthropology-of-religion
 
 echo opening the log file...
 
-"log_JVE_37-20.[philosophy].txt"
+"log_res_[60#].[theme=anthropology-of-religion].[20201112_163115].txt"
 
 echo opening the log file...complete
 
