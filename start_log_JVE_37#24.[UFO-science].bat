95b7ecb5c90891bd4cce6d1e7aeb1fc613258548
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-11-30 17:31:07 +0900

    (shortcuts_docs : JVE_37#24 : UFO-science) periodical updates

--- start_log_JVE_37#24.[UFO-science].bat
+++ start_log_JVE_37#24.[UFO-science].bat
@@ -1,12 +1,20 @@
 REM ===========================
+REM 	batch file ==> update
 REM 	folders
 REM 	doc
 REM 	app : mspaint
 REM 	git
 REM 	app : gimp
-REM 	batch file ==> update
 REM 	time label
 REM ===========================
+
+REM ===========================
+REM 	batch file ==> update
+REM 	2020年4月28日17:49:32
+REM ===========================
+REM echo.>> C:\Users\iwabuchiken\Desktop\shortcuts_docs\start_log_JVE_37#24.[UFO-science].bat
+echo.>> C:\WORKS_2\shortcuts_docs\start_log_JVE_37#24.[UFO-science].bat
+
 
 REM ===========================
 REM 	folders
@@ -19,35 +27,33 @@ REM 	doc
 REM 	2020年4月30日17:43:02
 REM ===========================
 REM text
-start C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\24_UFO-science\log_JVE_37-24.[UFO-science].odt
+rem : comment out 2020年11月30日17:03:24
+rem start C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\24_UFO-science\log_JVE_37-24.[UFO-science].odt
 
 REM sheet
-"C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\24_UFO-science\37#24.[UFO-science].[admin].ods"
+rem "C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\24_UFO-science\37#24.[UFO-science].[admin].ods"
+
+call "C:\WORKS_2\shortcuts_docs\res_JVE_37#24_UFO-science.txt"
 
 REM ===========================
 REM 	git
 REM 	2020年4月28日17:49:32
 REM ===========================
 REM C:\WORKS_2\Programs\Git_2.14.1\cmd\git.exe s
-pushd C:\WORKS_2\WS\WS_Others.JVEMV6
+rem pushd C:\WORKS_2\WS\WS_Others.JVEMV6
 REM C:\WORKS_2\batches\s.bat
-call C:\WORKS_2\batches\s.bat
+rem call C:\WORKS_2\batches\s.bat
 
 REM sakura C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\41_economics\7_topics\log_JVE_41.[economics].txt
 
-REM ===========================
-REM 	batch file ==> update
-REM 	2020年4月28日17:49:32
-REM ===========================
-REM echo.>> C:\Users\iwabuchiken\Desktop\shortcuts_docs\start_log_JVE_37#24.[UFO-science].bat
-echo.>> C:\WORKS_2\shortcuts_docs\start_log_JVE_37#24.[UFO-science].bat
 
 
 REM ===========================
 REM 	time label
 REM 	2020年5月27日15:44:27
 REM ===========================
-C:\WORKS_2\t.bat
+rem C:\WORKS_2\t.bat
+
 
 
 
