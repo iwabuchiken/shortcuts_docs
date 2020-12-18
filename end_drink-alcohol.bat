96f150055ddee0919549b4ec159ca0876ee6a130
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-12-19 01:26:07 +0900

    (shortcuts : log_[drink-alcohol]) periodical updates

---
+++ end_drink-alcohol.bat
@@ -0,0 +1,3 @@
+pushd C:\WORKS_2\shortcuts_docs && git add . && git commit -m "(shortcuts : log_[drink-alcohol]) periodical updates" && e && git push origin master && C:\WORKS_2\Utils\commands\push_fm.bat
+
+pause
