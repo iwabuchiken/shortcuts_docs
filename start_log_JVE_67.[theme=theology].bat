ca18bf90ac2c36bec7f2a7df6937f5b0e4020009
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-12-24 19:13:32 +0900

    (shortcuts : log_[flute]) periodical updates

--- start_log_JVE_67.[theme=theology].bat
+++ start_log_JVE_67.[theme=theology].bat
@@ -7,26 +7,26 @@ REM =================================
 @echo off
 
 REM opening message
-echo start_log_JVE_37-20.[philosophy].bat ==^> starting...
+echo start_log_JVE_67.[theme=theology].bat ==^> starting...
 
 rem goto end
 
 REM =================================
 REM 	update batch file
-REM 	2020å¹´10æœˆ24æ—¥13:20:36
+REM 	2020”N12ŒŽ24“ú13:46:41
 REM =================================
-echo.>> "C:\WORKS_2\shortcuts_docs\start_log_JVE_65_philosophy.[theme=philosophy].bat"
+echo.>> "C:\WORKS_2\shortcuts_docs\start_log_JVE_67.[theme=theology].bat"
 
 echo batch file --^> updated
 
 REM =================================
 REM 	browser
-REM 	2020å¹´11æœˆ2æ—¥15:02:53
+REM 	2020”N12ŒŽ24“ú13:46:49
 REM =================================
 
 pushd C:\WORKS_2\Programs\opera
 
-set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=jve+65+philosophy&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
+set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filter_memo=jve+67+theology&sort=file_name&direction=desc&RBs_AND_OR_Memo=AND"
 
 echo opening browser... : %url_1%
 
@@ -34,31 +34,24 @@ launcher.exe %url_1%
 
 REM =================================
 REM 	freemind
-REM 	2020å¹´11æœˆ2æ—¥15:03:29
+REM 	2020”N12ŒŽ24“ú13:46:53
 REM =================================
 echo starting... freemind
 
 rem "C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\37_miscs\20_philosophy\20_philosophy.mm"
-"C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\65_philosophy\7_philosophy-of-science\7_pos.mm"
+"C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\67_theology.mm\67_theology.mm"
 
 REM =================================
 REM 	folders
-REM 	2020å¹´11æœˆ2æ—¥15:08:45
+REM 	2020”N12ŒŽ24“ú13:46:57
 REM =================================
 start C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
 
 REM =================================
-REM 	log file
-REM 	2020å¹´10æœˆ22æ—¥10:03:40
+REM 	end
+REM 	2020”N12ŒŽ24“ú13:46:16
 REM =================================
-pushd C:\WORKS_2\shortcuts_docs
-
-echo opening the log file...
-
-"log_JVE_65.[theme=philosophy].txt"
-
-echo opening the log file...complete
-
 :end
 rem pause
+
 
