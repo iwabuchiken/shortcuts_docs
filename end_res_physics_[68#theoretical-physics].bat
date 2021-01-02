1dd817e7822dc2d2ead801b8a690c9a127228d64
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-01-03 01:28:03 +0900

    (shortcuts : log_[drink-alcohol]) periodical updates

--- end_res_physics_[68#theoretical-physics].bat
+++ end_res_physics_[68#theoretical-physics].bat
@@ -1,1 +1,4 @@
-taskkill /f /im javaw.exe && C:\WORKS_2\a && r f && g a . && g c -m "(JVE_68.[theme=theoretical-physics]) periodical updates" && e && p
+rem 2021”N1ŒŽ2“ú17:10:19 / write this "rem" lines
+rem : close apps ==> freemind
+rem : git ==> r f / r oj
+taskkill /f /im javaw.exe && C:\WORKS_2\a && r f && g a . && g c -m "(JVE_68.[theme=theoretical-physics]) periodical updates" && e && p && r oj && g a . && g c -m "(JVE_68.[theme=theoretical-physics]) periodical updates" && e && p
