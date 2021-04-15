b1bc149f4d1612f23f2ede88df1996d3426957e6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-04-15 17:43:23 +0900

    (JVE_73.[theme=ai]) periodical updates

--- end_log_JVE_73.[ai].bat
+++ end_log_JVE_73.[ai].bat
@@ -1,3 +1,1 @@
-set stringof_commit_message="(JVE_73.[theme=ai]) periodical updates"
-
-C:\WORKS_2\a && taskkill /f /im sakura.exe && taskkill /f /im javaw.exe && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m %stringof_commit_message% && e && p && r f && g a . && g c -m %stringof_commit_message% && e && p && r oj && g a . && g c -m %stringof_commit_message% && e && p
+C:\WORKS_2\a && taskkill /f /im sakura.exe && taskkill /f /im javaw.exe
