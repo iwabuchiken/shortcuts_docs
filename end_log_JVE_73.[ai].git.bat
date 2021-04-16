385de766acfbd0fca51dcc9824f7d256a9abb71c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-04-16 15:38:57 +0900

    (JVE_73.[theme=ai]) periodical updates

--- end_log_JVE_73.[ai].git.bat
+++ end_log_JVE_73.[ai].git.bat
@@ -1,3 +1,5 @@
 set stringof_commit_message="(JVE_73.[theme=ai]) periodical updates"
 
 C:\WORKS_2\a && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m %stringof_commit_message% && e && p && r f && g a . && g c -m %stringof_commit_message% && e && p && r oj && g a . && g c -m %stringof_commit_message% && e && p
+
+pause
--- start_log_JVE_73.[ai].bat
+++ start_log_JVE_73.[ai].bat
@@ -111,6 +111,8 @@ REM =================================
 rem ###
 start C:\WORKS_2\shortcuts_docs\end_log_JVE_64.[90_oritaku].bat
 
+start C:\WORKS_2\shortcuts_docs\end_log_JVE_73.[ai].git.bat
+
 set listof_apps=mspaint.exe sakura.exe rundll32.exe javaw.exe
 for %i in (%listof_apps%) do taskkill /f /im %i
 
