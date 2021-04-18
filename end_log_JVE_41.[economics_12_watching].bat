8ea4c9eb47152e1b54cbb1897a8bac64aeb7389b
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-04-18 13:49:40 +0900

    (JVE_73.[theme=ai]) periodical updates

---
+++ end_log_JVE_41.[economics_12_watching].bat
@@ -0,0 +1,1 @@
+taskkill /f /im javaw.exe && taskkill /f /im sakura.exe && C:\WORKS_2\a && r f && g a . && g c -m "(JVE_41.[theme=economics:topic=12_economy-watch]) periodical updates" && e && p
---
+++ end_log_JVE_63.[social-issues].bat
@@ -0,0 +1,12 @@
+set stringof_commit_memo="(JVE_63.[theme=social-issues:topic=%1]) periodical updates"
+
+echo commit memo : %stringof_commit_memo%
+
+rem pause
+
+rem C:\WORKS_2\a && r f && g a . && g c -m %stringof_commit_memo% && e && p
+
+taskkill /f /im javaw.exe && taskkill /f /im sakura.exe && C:\WORKS_2\a && r f && g a . && g c -m %stringof_commit_memo% && e && p
+
+
+rem taskkill /f /im javaw.exe && taskkill /f /im sakura.exe && C:\WORKS_2\a && r f && g a . && g c -m "(JVE_63.[theme=social-issues]) periodical updates" && e && p
