279a089d9151f107d7e9902216f452378e441790
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-06-08 14:30:59 +0900

    (sketch) periodical updates

--- end_sketch.bat
+++ end_sketch.bat
@@ -1,4 +1,8 @@
 rem 2021年6月7日11:42:07
 
-set stringof_labels="dessin"
+rem set stringof_labels="dessin"
+
+rem : change : 2021年6月8日14:28:18
+set stringof_labels="sketch"
+
 C:\WORKS_2\a && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p
