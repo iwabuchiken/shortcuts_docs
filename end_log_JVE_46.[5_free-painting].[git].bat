c15887167458ad402ef5cf7f58435c6555f10d1e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-06-21 17:31:38 +0900

    (JVE_46.[5_free-painting]) periodical updates

---
+++ end_log_JVE_46.[5_free-painting].[git].bat
@@ -0,0 +1,11 @@
+rem 2021年4月11日12:59:16
+rem : close apps ==> freemind
+rem : git ==> r f / r oj
+
+set stringof_message="JVE_46.[5_free-painting]"
+
+C:\WORKS_2\a && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p
+
+rem r oj && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p
+
+pause
--- start_visual-arts.[jve-46#6.5_free-paining].bat
+++ start_visual-arts.[jve-46#6.5_free-paining].bat
@@ -1,49 +1,90 @@
-REM ************************
+REM ==============================
+rem 	batch file --> update
 REM 	folders
 REM 	doc
 REM 	app : mspaint
 REM 	git
+rem 	freemind
 REM 	app : gimp
-REM ************************
+REM ==============================
+
+rem ==============================
+rem 	batch file --> update
+rem 	2021年6月21日16:29:53
+rem ==============================
+REM 
+echo.>> C:\Users\iwabuchiken\Desktop\shortcuts_docs\start_visual-arts.[jve-46#6.5_free-paining].bat
+
 
 REM goto testing
 
-REM ************************
+REM ==============================
 REM 	folders
-REM ************************
+REM ==============================
 start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\5_free-painting
 
 REM : C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+
 start C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
 
-REM ************************
-REM 	doc
-REM ************************
+
 REM : log
-start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\5_free-painting\46#6.5_free-painting.odt
+rem : comment out : 2021年6月21日16:27:13
+rem start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\5_free-painting\46#6.5_free-painting.odt
 
 REM : sheet
 REM start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\46#6_visual-arts.ods
-start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\5_free-painting\46#6.5_free-painting.ods
 
-REM ************************
+rem : comment out : 2021年6月21日16:27:13
+rem start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\5_free-painting\46#6.5_free-painting.ods
+
+REM ==============================
 REM 	app : mspaint
 REM 	2019/11/04 14:10:34
-REM ************************
-start mspaint.exe
+REM ==============================
+rem : comment out : 2021年6月21日16:27:13
+rem start mspaint.exe
 
-REM ************************
+REM ==============================
 REM 	git
 REM 	2020/04/28 15:45:34
-REM ************************
+REM ==============================
 REM C:\WORKS_2\Programs\Git_2.14.1\cmd\git.exe s
-pushd C:\WORKS_2\WS\WS_Others.Art
-REM C:\WORKS_2\batches\s.bat
-C:\WORKS_2\batches\s.bat && "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
 
-REM ************************
+rem : comment out : 2021年6月21日16:27:13
+rem pushd C:\WORKS_2\WS\WS_Others.Art
+
+REM C:\WORKS_2\batches\s.bat
+rem C:\WORKS_2\batches\s.bat && "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
+
+REM ==============================
+rem 	freemind
+rem 	2021年6月21日16:30:32
+REM ==============================
+echo starting... freemind
+
+set fpath_freemind="C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\46 Art\5_free-painting\5_free-painting.mm"
+
+echo "starting freemind... " %fpath_freemind%
+
+rem start 
+rem "%fpath_freemind%"
+%fpath_freemind%
+
+REM ==============================
 REM 	app : gimp
-REM ************************
+REM ==============================
+rem C:\WORKS_2\batches\s.bat gi
+
+echo calling C:\WORKS_2\batches\start_gimp.bat...
+
+rem C:\WORKS_2\batches\start_gimp.bat
+start C:\WORKS_2\batches\start_gimp.bat
+
+
+rem call "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
+
+
 REM :testing
 
 REM "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
@@ -52,5 +93,13 @@ REM call "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
 REM start "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"	==> prog doesn't start; console window opens #20191104_135645
 
 
-REM 
-echo.>> C:\Users\iwabuchiken\Desktop\shortcuts_docs\start_visual-arts.[jve-46#6.5_free-paining].bat
+REM ==============================
+REM 	doc
+REM ==============================
+"C:\WORKS_2\shortcuts_docs\log-session_JVE_46.[5_free-painting].log"
+
+rem call "C:\WORKS_2\shortcuts_docs\log-session_JVE_46.[5_free-painting].log"
+
+
+
+pause
