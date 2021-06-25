2b5cb7fa6d0763172f5682e208abb92c2db49cf6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-06-26 01:19:34 +0900

    (shortcuts : 1MPF_drink-alcohol) periodical updates

---
+++ end_drink-alcohol.1MPF_drink-alcohol.bat
@@ -0,0 +1,10 @@
+rem taskkill /f /im soffice.bin && taskkill /f /im javaw.exe && C:\WORKS_2\a.bat && git push origin master && C:\WORKS_2\Utils\commands\push_fm.bat && pushd C:\WORKS_2\shortcuts_docs && git add . && git commit -m "(shortcuts : log_[drink-alcohol]) periodical updates" && e
+
+rem : soffice ==> remove from the line --> b/c when force to quit, the openoffice files get corrupted. 2020”N12ŒŽ31“ú0:56:47
+rem taskkill /f /im javaw.exe && C:\WORKS_2\a.bat && git push origin master && C:\WORKS_2\Utils\commands\push_fm.bat && pushd C:\WORKS_2\shortcuts_docs && git add . && git commit -m "(shortcuts : log_[drink-alcohol]) periodical updates" && e
+
+set message_string="(shortcuts : 1MPF_drink-alcohol) periodical updates"
+
+C:\WORKS_2\a.bat && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m %message_string% && e && r oj && g a . && g c -m %message_string% && e && p
+
+pause
