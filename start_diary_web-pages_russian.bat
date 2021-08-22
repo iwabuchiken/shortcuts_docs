895beae4d3e45f2b81fbeb828653873891ebbbc8
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-08-22 14:12:17 +0900

    (JVE_41#12_watching) periodical updates

--- start_diary_web-pages_russian.bat
+++ start_diary_web-pages_russian.bat
@@ -8,14 +8,14 @@ rem pushd C:\WORKS_2\Programs\opera
 pushd "C:\Program Files\Mozilla Firefox"
 
 rem polish - english
-set url_trans_ru_en="https://translate.google.co.jp/?hl=ja&sl=es&tl=en&op=translate"
-set url_trans_en_ru="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=es"
+set url_trans_ru_en="https://translate.google.co.jp/?hl=ja&sl=ru&tl=en&op=translate"
+set url_trans_en_ru="https://translate.google.co.jp/?hl=ja#view=home&op=translate&sl=en&tl=ru"
 
-rem : 2021年2月24日10:58:03
-rem set url_polish_grammar="https://ja.wikipedia.org/wiki/ポーランド語#格変化"
+rem : 2021年8月21日12:00:21
+set url_polish_grammar="https://ja.wikipedia.org/wiki/ロシア語#格変化"
 
 
-set list_of_url=%url_trans_ja_ko% %url_trans_ko_ja% %url_trans_en_ru% %url_trans_ru_en% %url_trans_vi_en% %url_trans_en_vi% %url_forvo_persian% %url_polish_grammar% %url_random_number%
+set list_of_url=%url_trans_en_ru% %url_trans_ru_en% %url_polish_grammar%
 
 rem : launch
 rem launcher.exe  %list_of_url%
