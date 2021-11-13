335e2174a62bf4631719de3a440f86b277f8f51f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-11-14 08:57:48 +0900

    (JVE_46-6-8_japanese-art) periodical updates

---
+++ end_log_JVE_46-6-8_japanese-art.git.bat
@@ -0,0 +1,9 @@
+rem 2021年11月14日8:33:22
+rem : close apps ==> freemind
+rem : git ==> r f / r oj
+
+rem C:\WORKS_2\shortcuts_docs\end_log_JVE_46-6-8_japanese-art_git.bat
+
+set stringof_message="JVE_46-6-8_japanese-art"
+
+C:\WORKS_2\a && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p && r f && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p && r a && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p
