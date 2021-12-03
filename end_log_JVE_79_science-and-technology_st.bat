9701338c53a90ad64928e942655657e2f477db84
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-12-03 09:43:53 +0900

    (JVE_79.[science-and-technology_st]) periodical updates

---
+++ end_log_JVE_79_science-and-technology_st.bat
@@ -0,0 +1,9 @@
+set stringof_labels="JVE_79.[science-and-technology_st]"
+
+C:\WORKS_2\a && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && r f && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && r oj && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p
+rem C:\WORKS_2\a && r f && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && r oj && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p
+
+rem  && taskkill /f /im sakura.exe && taskkill /f /im javaw.exe
+
+pause
+rem C:\WORKS_2\shortcuts_docs\end_log_JVE_79_science-and-technology_st.bat
