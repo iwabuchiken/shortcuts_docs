3055ef7ef7a6e3e3d661da09c8f717afce3390fd
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-12-03 09:48:26 +0900

    (JVE_79.[science-and-technology_st]) periodical updates

--- end_log_JVE_79_science-and-technology_st.bat
+++ end_log_JVE_79_science-and-technology_st.bat
@@ -1,6 +1,6 @@
 set stringof_labels="JVE_79.[science-and-technology_st]"
 
-C:\WORKS_2\a && echo "changing dir to ...\\shortcuts_docs" && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && echo "git push... complete" && echo "changing dir to ...\FM..." && r f && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && echo "git push... complete" && echo "changing dir to ...\\WS_Others.JVEMV6" && r oj && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && echo "git push... complete"
+C:\WORKS_2\a && echo "changing dir to ...\\shortcuts_docs" && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && echo "git push... complete" && echo "changing dir to ...\FM..." && r f && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && echo "git push... complete" && echo "changing dir to ...\\WS_Others.JVEMV6" && r oj && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && echo "git push... complete" && pause
 
 rem C:\WORKS_2\a && r f && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p && r oj && g a . && g c -m "(%stringof_labels:"=%) periodical updates" && e && p
 
