6787331eb94844bbc18098929def44a3fe1beed4
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-03-16 12:33:17 +0900

    diary

---
+++ start_lang_ancient_greek.bat
@@ -0,0 +1,40 @@
+REM =================================
+REM 	a.bat
+REM 	browser
+REM 	batch file
+REM 	docs
+REM =================================
+
+
+REM =================================
+REM 	a.bat
+REM 	2022年3月15日11:00:18
+REM =================================
+rem C:\WORKS_2\a.bat
+
+REM =================================
+REM 	browser
+REM 	2022年2月17日10:03:45
+REM =================================
+
+
+rem start C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\9_prog_lang\storage_9_prog_lang\start_web-pages_hebrew.bat
+
+rem sakura C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\9_prog_lang\storage_9_prog_lang\start_web-pages_hebrew.bat
+
+
+REM =================================
+REM 	docs
+REM 	2022年2月17日10:04:00
+REM =================================
+start C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\9_prog_lang\storage_9_prog_lang\ancient_greek.odt
+
+REM =================================
+REM 	batch file
+REM 	2022年3月15日10:55:55
+REM =================================
+rem C:\WORKS_2\Programs\sakura\sakura.exe C:\WORKS_2\shortcuts_docs\start_lang_ancient_greek.bat
+C:\WORKS_2\Programs\sakura\sakura.exe C:\WORKS_2\shortcuts_docs\start_lang_ancient_greek.bat
+
+pause
+rem exit
--- start_lang_hebrew.bat
+++ start_lang_hebrew.bat
@@ -11,6 +11,8 @@ REM =================================
 
 start C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\9_prog_lang\storage_9_prog_lang\start_web-pages_hebrew.bat
 
+sakura C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\9_prog_lang\storage_9_prog_lang\start_web-pages_hebrew.bat
+
 REM =================================
 REM 	docs
 REM 	2022”N2ŒŽ17“ú10:04:00
