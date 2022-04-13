87700052bf3e46c1634365d6683f95e271fd780f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-04-13 16:31:40 +0900

    (log_JVE_46-6-8_japanese-art) periodical updates

---
+++ start_JVE_46-6-8_japanese-art.bat
@@ -0,0 +1,96 @@
+REM ==============================
+rem 	message : opening
+rem 	batch file --> update
+REM 	folders
+REM 	doc
+REM 	app : mspaint
+REM 	git
+rem 	freemind
+REM 	app : gimp
+REM ==============================
+
+rem ==============================
+rem 	message : opening
+rem 	2022年4月13日14:58:08
+rem ==============================
+echo staritng file.. : C:\WORKS_2\shortcuts_docs\start_JVE_46-6-8_japanese-art.bat
+
+
+rem ==============================
+rem 	batch file --> update
+rem 	2021年6月21日16:29:53
+rem ==============================
+REM 
+
+echo.>> C:\WORKS_2\shortcuts_docs\start_JVE_46-6-8_japanese-art.bat
+
+
+REM goto testing
+
+REM ==============================
+REM 	folders
+REM ==============================
+start C:\WORKS_2\WS\WS_Others.Art\JVEMV6\46_art\6_visual-arts\8_japanese-art
+
+start C:\WORKS_2\WS\WS_Cake_IFM11\iphone_to_upload
+
+
+REM ==============================
+REM 	app : mspaint
+REM 	2019/11/04 14:10:34
+REM ==============================
+rem : comment out : 2021年6月21日16:27:13
+rem start mspaint.exe
+
+REM ==============================
+REM 	git
+REM 	2020/04/28 15:45:34
+REM ==============================
+REM C:\WORKS_2\Programs\Git_2.14.1\cmd\git.exe s
+
+rem : comment out : 2021年6月21日16:27:13
+rem pushd C:\WORKS_2\WS\WS_Others.Art
+
+REM C:\WORKS_2\batches\s.bat
+rem C:\WORKS_2\batches\s.bat && "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
+
+rem : 2022年4月13日14:59:31
+rem "C:\WORKS_2\Programs\GIMP 2\bin\gimp-2.8.exe"
+
+REM ==============================
+rem 	freemind
+rem 	2021年6月21日16:30:32
+REM ==============================
+rem echo starting... freemind
+
+rem set fpath_freemind="C:\WORKS_2\WS\FM_2_20171104_225946\Projects\JVEMV6\46 Art\5_free-painting\5_free-painting.mm"
+
+rem echo "starting freemind... " %fpath_freemind%
+
+rem start 
+rem "%fpath_freemind%"
+rem %fpath_freemind%
+
+REM ==============================
+REM 	app : gimp
+REM ==============================
+rem C:\WORKS_2\batches\s.bat gi
+
+echo calling C:\WORKS_2\batches\start_gimp.bat...
+
+rem C:\WORKS_2\batches\start_gimp.bat
+start C:\WORKS_2\batches\start_gimp.bat
+
+
+REM ==============================
+REM 	doc
+REM ==============================
+C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\shortcuts_docs\log_JVE_46-6-8_japanese-art.log"
+
+
+
+
+
+pause
+
+
