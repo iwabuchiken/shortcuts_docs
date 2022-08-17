e452e2805901341b9c34f68b4d2861d0b59afdaf
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-08-17 18:12:51 +0900

    (JVE_46_jp-flute) periodical updates

--- log_files/start_music-composition_v2.bat
+++ log_files/start_music-composition_v2.bat
@@ -27,11 +27,6 @@ rem 	2022”N8ŒŽ9“ú13:33:17
 rem ===============================
 rem start C:\Users\iwabuchiken\VLC\vlc.exe
 
-rem ===============================
-rem 	log file
-rem 	2022”N8ŒŽ9“ú13:33:17
-rem ===============================
-C:\WORKS_2\shortcuts_docs\t.20220429_161510.buffer-file-for-list-of-books.txt.bat && C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\shortcuts_docs\log_files\log_jap-flute_shino.log"
 
 rem ===============================
 rem 	musescore
@@ -53,6 +48,11 @@ set url_1="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/index_2?filte
 
 browser.exe %url_1%
 
+rem ===============================
+rem 	log file
+rem 	2022”N8ŒŽ9“ú13:33:17
+rem ===============================
+C:\WORKS_2\shortcuts_docs\t.20220429_161510.buffer-file-for-list-of-books.txt.bat && C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\shortcuts_docs\log_files\log_jap-flute_shino.log"
 
 
 
---
+++ log_files/start_music-composition_v2.open-log-files.bat
@@ -0,0 +1,10 @@
+@echo on
+rem ===============================
+rem 	log file
+rem 	2022年8月9日13:33:17
+rem ===============================
+C:\WORKS_2\shortcuts_docs\t.20220429_161510.buffer-file-for-list-of-books.txt.bat && C:\WORKS_2\Programs\sakura\sakura.exe "C:\WORKS_2\shortcuts_docs\log_files\log_jap-flute_shino.log"
+
+
+
+pause
