13cf775c51314ba64d7bf6478c3212ea86fce72a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-08-25 15:46:00 +0900

    JVE_46_jp-flute :ﾂshino: compose

--- end_batch/end_log_jap-flute_shino.log.bat
+++ end_batch/end_log_jap-flute_shino.log.bat
@@ -4,13 +4,14 @@ rem 	C:\WORKS_2\shortcuts_docs\end_batch\end_log_jap-flute_shino.log.bat
 rem 	from : C:\WORKS_2\shortcuts_docs\log_files\log_jap-flute_shino.log		##mark:20220825_154200
 rem ==============================
 
+rem ------------------------------- git : WS_Others.Art
+rem //2022年8月20日14:49:18
+set stringof_commit_message="JVE_46_jp-flute : shino: compose"
+C:\WORKS_2\a && r a && g a . && g c -m %stringof_commit_message% && e && p && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m %stringof_commit_message%
+
 set stringof_session_id="shino: compose"
 
 set stringof_project_id="JVE_46_jp-flute : %stringof_session_id:"=%"
 
 C:\WORKS_2\shortcuts_docs\log_files\end_log_JVE_ALL.[ai].git.bat %stringof_project_id:"=%
 
-rem ------------------------------- git : WS_Others.Art
-rem //2022年8月20日14:49:18
-set stringof_commit_message="JVE_46_jp-flute : shino: compose"
-C:\WORKS_2\a && r a && g a . && g c -m %stringof_commit_message% && e && p && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m %stringof_commit_message%
--- log_files/start_log_JVE_76-physics.bat
+++ log_files/start_log_JVE_76-physics.bat
@@ -697,3 +697,4 @@ rem pause
 
 
 
+
