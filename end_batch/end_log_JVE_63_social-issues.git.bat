b122d2028aa5554b918e6111ee0c2ff9826a1ef6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-10-11 17:09:21 +0900

    JVE_72.[read] : res:history:id=XOL4

---
+++ end_batch/end_log_JVE_63_social-issues.git.bat
@@ -0,0 +1,16 @@
+rem ============================
+
+rem 	2022年10月3日15:39:55
+rem 	file : C:\WORKS_2\shortcuts_docs\end_batch\end_log_JVE_63_social-issues.git.bat
+
+rem : close apps ==> freemind
+rem : git ==> r f / r oj
+rem ============================
+
+set stringof_message="JVE_63_social-issues"
+
+C:\WORKS_2\a && r f && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p && r oj && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p
+
+
+rem C:\WORKS_2\shortcuts_docs\log_files\end_log_JVE_68.[tp_v-2.0].[git].bat
+
