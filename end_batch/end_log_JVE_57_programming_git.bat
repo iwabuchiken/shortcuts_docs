52f1b0a3d246c2a79851d6b28e6360c586d3395e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-10-25 12:34:18 +0900

    (JVE_57_programming) periodical updates

---
+++ end_batch/end_log_JVE_57_programming_git.bat
@@ -0,0 +1,10 @@
+rem // 2022年10月25日12:30:01
+rem : close apps ==> freemind
+rem : git ==> r f / r oj
+rem // C:\WORKS_2\shortcuts_docs\end_batch\end_log_JVE_57_programming_git.bat
+
+set stringof_message="JVE_57_programming"
+
+C:\WORKS_2\a && r f && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p && r oj && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p && r s1 && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p
+
+
---
+++ end_batch/end_log_JVE_83_plastics_git.bat
@@ -0,0 +1,10 @@
+rem // 2022年10月13日16:41:33
+rem : close apps ==> freemind
+rem : git ==> r f / r oj
+rem // C:\WORKS_2\shortcuts_docs\end_batch\end_log_JVE_83_plastics_git.bat
+
+set stringof_message="JVE_83_plastics"
+
+C:\WORKS_2\a && r f && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p && r oj && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p
+
+
