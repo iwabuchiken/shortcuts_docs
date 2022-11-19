bd061cbfa490a34d454faabe1ed00817bf94fbad
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-11-19 15:17:11 +0900

    JVE_46_jp-flute :ﾂshino

--- end_batch/end_log_jap-flute_shino.log.v2.bat
+++ end_batch/end_log_jap-flute_shino.log.v2.bat
@@ -3,9 +3,10 @@ rem 	2022年8月30日11:41:55
 rem 	C:\WORKS_2\shortcuts_docs\end_batch\end_log_jap-flute_shino.log.v2.bat
 rem ==============================
 
-set stringof_commit_message="JVE_46_jp-flute : shino: play"
+set stringof_commit_message="JVE_46_jp-flute : shino"
+rem set stringof_commit_message="JVE_46_jp-flute : shino: play"
 
-set stringof_commit_message="JVE_46_jp-flute : shino: compose"
+rem set stringof_commit_message="JVE_46_jp-flute : shino: compose"
 
 C:\WORKS_2\a.bat && pushd C:\WORKS_2\shortcuts_docs && g a . && g c -m %stringof_commit_message% && C:\WORKS_2\a && r a && g a . && g c -m %stringof_commit_message% && e && p && pause
 
