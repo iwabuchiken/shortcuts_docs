70b369440372d51b8e992196837c5e40090693aa
iwabuchiken <iwabuchi.k.2010@gmail.com>
2023-01-21 16:41:48 +0900

    (git : shortcuts_docs) periodical updates

---
+++ end_batch/git_shortcut-docs_directory.bat
@@ -0,0 +1,13 @@
+rem // 2022年10月13日16:41:33
+rem : close apps ==> freemind
+rem : git ==> r f / r oj
+rem // C:\WORKS_2\shortcuts_docs\end_batch\end_log_JVE_83_plastics_git.bat
+
+set stringof_message="git : shortcuts_docs"
+
+C:\WORKS_2\a && r s1 && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p && r f && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p && r oj && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p
+
+rem / c.o. 2022年11月25日14:44:54
+rem C:\WORKS_2\a && r f && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p && r oj && g a . && g c -m "(%stringof_message:"=%) periodical updates" && e && p
+
+
